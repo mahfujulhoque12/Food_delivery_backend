94fa5e7e7360ce94a2364117a0344b4555fd4964
mahfujulhoque12 <mdmahfujulhoque123@gmail.com>
2026-08-02 22:25:48 +0600

    smtp change

--- utils/mail.js
+++ utils/mail.js
@@ -10,8 +10,8 @@ console.log({
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   family: 4,
   auth: {
     user: process.env.SMTP_USER,
